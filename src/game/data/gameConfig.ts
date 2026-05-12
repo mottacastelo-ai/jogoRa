2009cf64a26d6512155967dba475e2f159b333bf
mottacastelo-ai <wizardcastelo@gmail.com>
2026-05-11 21:59:00 -0300

    Add shared game configuration

---
+++ src/game/data/gameConfig.ts
@@ -0,0 +1,21 @@
+export type PlayerCount = 2 | 3 | 4
+
+export const DEFAULT_PLAYER_COUNT: PlayerCount = 4
+
+export const PLAYER_COLORS = [
+  0xe74c3c,
+  0x3498db,
+  0x2ecc71,
+  0xf1c40f
+]
+
+export const PLAYER_SYMBOLS = ['☀', '𓂀', '☥', '★']
+
+export const DANGER_CELL_TYPES = [
+  'apophis',
+  'apophisStrong',
+  'darkness',
+  'fire',
+  'chains',
+  'labyrinth'
+]
